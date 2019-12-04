b14431eeba7e9c455c586e5cfd86e57c9586c264
Mohit Gupta <mgupta4@kamek.(none)>
2019-12-03 19:14:02 -0600

    hash

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -7,8 +7,15 @@
 #include <fstream>
 #include <queue>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+typedef struct Node {
+	int index;
+	struct Node* next;
+} Node;
+
 
 /*function... might want it in some class?*/
 int getdir (string dir, vector<string> &files)
@@ -47,9 +54,39 @@ void readWords(string dir, string fileName) {
 	}
 }
 
-void printChunks(string dir, string fileName) {
+unsigned long hashChunk(vector<string> chunk){
+	string megaChunk = "";
+	unsigned long power = 1;
+	unsigned long sum = 0;
+	for(int i = 0; i < chunk.size(); i++){
+		megaChunk = megaChunk + chunk[i];
+	}
+	for(int i = 0; i < megaChunk.length(); i++){
+		sum += (unsigned long)(megaChunk[i]*power);
+		power = power * 7;
+	}
+	sum = sum % 1500007;
+	return sum;
+}
+
+void addNode(Node* hashTable[], int index, int fileNum) {
+	Node* entry = new Node;
+	entry->index = fileNum;
+	entry->next = NULL;
+	if(hashTable[index] == NULL){
+		hashTable[index] = entry;
+	} 
+	else {
+		entry->next = hashTable[index];
+		hashTable[index] = entry;
+	}
+}	
+	
+
+void printChunks(string dir, string fileName, Node* hashTable[], int fileNum) {
 	std::queue<string> chunks;
 	string word;
+	int index = 0;
 	ifstream myfiles((dir + "/" + fileName).c_str());
 	if(myfiles.is_open()) {
 		while(myfiles >> word) {
@@ -67,37 +104,57 @@ void printChunks(string dir, string fileName) {
 			line.push_back(chunks.front());
 			chunks.pop();
 		}
+
+		index = hashChunk(line);
+		addNode(hashTable, index, fileNum);
+
+		/*
 		for(int i = 0; i < line.size(); i++) {
 			cout << line[i] << " ";
 		}
+		*/		
+
 		while(chunks.size() > 0) {
 			line.erase(line.begin());
 			line.push_back(chunks.front());
 			chunks.pop();
+			index = hashChunk(line);
+			addNode(hashTable, index, fileNum);
+			/*
 			for(int i = 0; i < line.size(); i++) {
 				cout << line[i] << " ";
 			}
 			cout << endl;
+			*/
 		}		
+		
 	}
 }	
 
+
 int main()
-{
+{   
+	Node* hashTable[1500007];
     string dir = string("sm_doc_set");
     vector<string> files = vector<string>();
 
     getdir(dir,files);
 	files.erase(files.begin());
 	files.erase(files.begin());	
-
+/*
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
     }
+*/	
+	for(int i = 0; i<1500007; i++){
+		hashTable[i] = NULL;
+	}
+		
 	
 //	readFile(dir, files[0]);    
 //	readWords(dir, files[0]);
-	printChunks(dir, files[1]);	
-	
+	int fileNum = 1;
+	printChunks(dir, files[1], hashTable, fileNum);	
+	int x = 0;	
 	return 0;
 }
